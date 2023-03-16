ec0dccded353ab0440cd96b1bfbddfb3f9ddcaa3
YuliyaTonkikh <uliya_i@bk.ru>
2023-03-16 21:22:10 +0700

    fix

--- src/test/java/ru/netology/delivery/CardDeliveryTest.java
+++ src/test/java/ru/netology/delivery/CardDeliveryTest.java
@@ -3,7 +3,6 @@ package ru.netology.delivery;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
