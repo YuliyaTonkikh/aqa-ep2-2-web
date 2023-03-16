2c04faddfee6bde526fd58935345a03845d350e3
YuliyaTonkikh <uliya_i@bk.ru>
2023-03-16 12:26:08 +0700

    fix

--- src/test/java/ru/netology/delivery/CardDeliveryTest.java
+++ src/test/java/ru/netology/delivery/CardDeliveryTest.java
@@ -18,10 +18,15 @@ import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CardDeliveryTest {
+
+    @BeforeEach
+    public void setUp() {
+        open("http://localhost:9999");
+    }
+
     @Test
     @DisplayName("Успешная отправка формы доставки карты")
     public void shouldSendForm() {
-        open("http://localhost:9999");
         String deliveryDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
 
         $("[data-test-id=city] input").setValue("Абакан");
@@ -35,6 +40,22 @@ class CardDeliveryTest {
         $("[data-test-id=notification]").shouldHave(Condition.text
                 ("Успешно! Встреча успешно забронирована на " + deliveryDate), Duration.ofSeconds(15));
 
+    }
+
+    @Test
+    @DisplayName("Ошибка при отправке формы доставки карты с пустым городом")
+    public void shouldNotSendCity() {
+        String deliveryDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE);
+        $("[data-test-id=date] input").setValue(deliveryDate);
+        $("[data-test-id=name] input").setValue("Тонких Юлия");
+        $("[data-test-id=phone] input").setValue("+79999990000");
+        $("[data-test-id=agreement] span").click();
+        $(withText("Забронировать")).click();
+        String expectedText = "Поле обязательно для заполнения";
+        String actualText = $("[data-test-id=city] .input__sub").getText().trim();
+        assertEquals(expectedText, actualText);
     }
 
 }
